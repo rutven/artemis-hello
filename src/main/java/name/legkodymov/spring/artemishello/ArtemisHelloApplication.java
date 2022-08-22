4d36e320090b2899ecf5b9495faa9213d939ad25
Sergey <rutven@gmail.com>
2022-08-22 19:03:46 +0300

    Spring starter

---
+++ src/main/java/name/legkodymov/spring/artemishello/ArtemisHelloApplication.java
@@ -0,0 +1,13 @@
+package name.legkodymov.spring.artemishello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemisHelloApplication.class, args);
+	}
+
+}
